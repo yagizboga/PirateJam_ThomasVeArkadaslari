ff7d15a29e3c0d7a0a24a9802d7fbe136381c415
Mert <122644315+mertkavilcioglu@users.noreply.github.com>
2025-01-21 20:48:47 +0300

    Dig Anim Update

--- Assets/Scripts/CoalMan/CoalPlayer.cs
+++ Assets/Scripts/CoalMan/CoalPlayer.cs
@@ -1,26 +1,66 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CoalPlayer : MonoBehaviour
 {
     private bool isAttacking = false;
+    private bool isDigging = false;
     private Animator animator;
     private bool isHoldingShovel = true;
+    private bool canDig = true;
+    private bool isReadyToCarry = false;
+
+    public GameObject attackShovel;
+    public GameObject diggingShovel;
     private void Start()
     {
         animator = GetComponent<Animator>();
+        attackShovel.SetActive(true);
+        diggingShovel.SetActive(false);
     }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            if (!isAttacking && isHoldingShovel)
+            if (!isAttacking && isHoldingShovel && !isDigging)
             {
                 isAttacking = true;
-                animator.SetBool("isShovelAttacking", isAttacking);
-
+                animator.SetBool("isShovelAttacking", true);
+                if ((attackShovel.activeSelf == false) || diggingShovel.activeSelf == true)
+                {
+                    if (!isDigging)
+                    {
+                        attackShovel.SetActive(true);
+                        diggingShovel.SetActive(false);
+                    }
+                }
             }
-            
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            if (!isAttacking && isHoldingShovel && !isDigging && canDig)
+            {
+                canDig = false;
+                isDigging = true;
+                animator.SetBool("isDigging", true);
+                if ((attackShovel.activeSelf == true) || diggingShovel.activeSelf == false)
+                {
+                    if (!isAttacking)
+                    {
+                        attackShovel.SetActive(false);
+                        diggingShovel.SetActive(true);
+                    }
+                }
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            if (isDigging)
+            {
+                isDigging = false;
+                animator.SetBool("isDigging", false);
+            }
         }
     }
 
@@ -32,6 +72,42 @@ public class CoalPlayer : MonoBehaviour
     public void SetIsAttackingFalse() 
     { 
         isAttacking = false;
-        animator.SetBool("isShovelAttacking", isAttacking);
+        animator.SetBool("isShovelAttacking", false);
     }
-}
+
+    public void SetIsDiggingFalse()
+    {
+        isDigging = false;
+        animator.SetBool("isDigging", false);
+    }
+
+    public void SetAttackShovel()
+    {
+        attackShovel.SetActive(true);
+        diggingShovel.SetActive(false);
+    }
+
+    public void SetDigShovel()
+    {
+        attackShovel.SetActive(false);
+        diggingShovel.SetActive(true);
+    }
+
+    public void SetCanDigTrue()
+    {
+        canDig = true;
+        StartCoroutine(ResetCanDig());
+    }
+
+    private IEnumerator ResetCanDig()
+    {
+        yield return new WaitForSeconds(0.15f);
+        canDig = true;
+    }
+
+    public void SetIsReadyToCarryTrue()
+    {
+        isReadyToCarry = true;
+        animator.SetBool("isReadyToCarry", true);
+    }
+}   
