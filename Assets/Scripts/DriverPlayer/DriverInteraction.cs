c8c7b295d056fd594c51c801f6553c0fa0613f53
Mert Kavilcioğlu <mertkavilci@gmail.com>
2025-01-29 00:29:56 +0300

    Raycast Interaction Update

--- Assets/Scripts/DriverPlayer/DriverInteraction.cs
+++ Assets/Scripts/DriverPlayer/DriverInteraction.cs
@@ -2,40 +2,85 @@ using UnityEngine;
 
 public class DriverInteraction : MonoBehaviour
 {
+    public float interactionDistance = 3f;   
+    public float sphereRadius = 0.5f;       
+    public Camera playerCamera;            
 
-    public float interactionDistance = 3f;
-    public Camera playerCamera;
+    private GameObject lastHighlightedObject; 
+    private Material originalMaterial;       
+    public Material highlightMaterial;       
 
     void Update()
     {
-        Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, interactionDistance))
+        if (Physics.SphereCast(playerCamera.transform.position, sphereRadius, playerCamera.transform.forward, out hit, interactionDistance))
         {
-            if (hit.collider.CompareTag("Gas"))
+            GameObject hitObject = hit.collider.gameObject;
+
+            if (hitObject.CompareTag("Gas") || hitObject.CompareTag("Brake"))
             {
+                HighlightObject(hitObject); 
+
                 if (Input.GetKeyDown(KeyCode.E))
                 {
-                    Debug.Log("Gas interacted!");
+                    if (hitObject.CompareTag("Gas"))
+                    {
+                        Debug.Log("Gas interacted!");
+                    }
+                    else if (hitObject.CompareTag("Brake"))
+                    {
+                        Debug.Log("Brake interacted!");
+                    }
                 }
             }
-            else if (hit.collider.CompareTag("Brake"))
+            else
             {
-                if (Input.GetKeyDown(KeyCode.E))
-                {
-                    Debug.Log("Brake interacted!");
-                }
+                ClearHighlight();
             }
+        }
+        else
+        {
+            ClearHighlight();
         }
     }
 
-    void OnDrawGizmos()
+    private void HighlightObject(GameObject obj)
+    {
+        if (lastHighlightedObject == obj) return; 
+
+        ClearHighlight(); 
+
+        Renderer objRenderer = obj.GetComponent<Renderer>();
+        if (objRenderer != null)
+        {
+            originalMaterial = objRenderer.material;
+            objRenderer.material = highlightMaterial; 
+            lastHighlightedObject = obj;
+        }
+    }
+
+    private void ClearHighlight()
+    {
+        if (lastHighlightedObject != null)
+        {
+            Renderer objRenderer = lastHighlightedObject.GetComponent<Renderer>();
+            if (objRenderer != null && originalMaterial != null)
+            {
+                objRenderer.material = originalMaterial;
+            }
+            lastHighlightedObject = null;
+            originalMaterial = null;
+        }
+    }
+
+    private void OnDrawGizmos()
     {
         if (playerCamera != null)
         {
             Gizmos.color = Color.red;
             Gizmos.DrawRay(playerCamera.transform.position, playerCamera.transform.forward * interactionDistance);
+            Gizmos.DrawWireSphere(playerCamera.transform.position + playerCamera.transform.forward * interactionDistance, sphereRadius);
         }
     }
 }
