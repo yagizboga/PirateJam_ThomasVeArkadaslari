65a209e3ce2b81c1740d63c27e62551419c46a16
Mert Kavilcioğlu <mertkavilci@gmail.com>
2025-01-29 15:12:59 +0300

    EnemyMovement Update1

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -6,24 +6,29 @@ using UnityEngine.AI;
 public class EnemyMovement : MonoBehaviour
 {
     private NavMeshAgent agent;
-    private Transform shooter;
+    private Transform shooterTransform;
+    private Transform coalPlayerTransform;
+    private Transform driverPlayerTransform;
+
+
     [SerializeField] Animator animator;
 
-
+    //y aralalýðýnda shooter varsa ve diger carlardan daha yakýndaysa shootera, yoksa kürekci veya driverdn hangisi yakýnsa ona
     void Awake(){
         agent = GetComponent<NavMeshAgent>();
     }
     void Start()
     {
-        shooter = GameObject.FindGameObjectWithTag("shooter").transform;
+        shooterTransform = GameObject.FindGameObjectWithTag("shooter").transform;
+        coalPlayerTransform = GameObject.FindGameObjectWithTag("CoalPlayer").transform;
+        driverPlayerTransform = GameObject.FindGameObjectWithTag("DriverPlayer").transform;
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        if(shooter != null)
+        if(shooterTransform != null)
         {
-            agent.SetDestination(shooter.position);
+            agent.SetDestination(shooterTransform.position);
         }
         animator.SetFloat("speed",agent.velocity.magnitude);
     }
