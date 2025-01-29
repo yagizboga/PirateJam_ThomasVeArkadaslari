932063337d0798296e2eb07b279fdb141a1b3ecd
Yağız Boğa <110109838+yagizboga@users.noreply.github.com>
2025-01-30 00:40:22 +0300

    komit

--- Assets/Scripts/Environment/repairtrigger.cs
+++ Assets/Scripts/Environment/repairtrigger.cs
@@ -2,15 +2,9 @@ using UnityEngine;
 
 public class repairtrigger : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnTriggerEnter(Collider other){
+        if(other.CompareTag("trainfront")){
+            GameObject.FindGameObjectWithTag("repairtrigger").GetComponent<RepairTrigger>().SetIsBroken(true);
+        }
     }
 }
