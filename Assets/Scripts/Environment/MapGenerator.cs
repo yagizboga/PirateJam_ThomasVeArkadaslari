52daa6bb6c199060a1da9d9d6f977e90dc24fcf9
Yağız Boğa <110109838+yagizboga@users.noreply.github.com>
2025-01-29 05:25:12 +0300

    upupup

--- Assets/Scripts/Environment/MapGenerator.cs
+++ Assets/Scripts/Environment/MapGenerator.cs
@@ -1,43 +1,50 @@
 using System.Collections.Generic;
 using System.Diagnostics.Tracing;
+using System.Threading;
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    [SerializeField] GameObject terrain0;
-    [SerializeField] GameObject terrain1;
-    List<GameObject> maps;
-    Grid grid;
 
-    private float speedInit = 10f;
+    [SerializeField] List<GameObject> maps;
+    List<GameObject> activemaps;
+    Grid grid;
+    int count = 1;
+
+    private float speedInit = 0f;
     private float acceleration = 10f;
     private float finalSpeed;
     void Start(){
-        maps = new List<GameObject>();
-        grid = new Grid(5,1,200);
-        maps.Add(Instantiate(terrain0,grid.GetMap(0,0),Quaternion.identity,this.transform));
-        maps.Add(Instantiate(terrain1,grid.GetMap(1,0),Quaternion.identity,this.transform));
-        maps.Add(Instantiate(terrain1, grid.GetMap(2, 0), Quaternion.identity, this.transform));
-        maps.Add(Instantiate(terrain1, grid.GetMap(3, 0), Quaternion.identity, this.transform));
-        maps.Add(Instantiate(terrain1, grid.GetMap(4, 0), Quaternion.identity, this.transform));
+        activemaps = new List<GameObject>();
+        grid = new Grid(2,1,1000);
+        activemaps.Add(Instantiate(maps[0],grid.GetMap(1,0),Quaternion.identity,this.transform));
     }
 
     public void GenerateNextMap(){
-        maps.Add(Instantiate(terrain1,grid.GetMap(4,0),Quaternion.identity,this.transform));
-        Debug.Log(grid.GetMap(4,0));
+        activemaps.Add(Instantiate(maps[count],grid.GetMap(0,0),Quaternion.identity,this.transform));
+        count+=1;
     }
 
     void FixedUpdate(){
         if(Input.GetKey("w")){
+            if(speedInit <5){
+                speedInit+=Time.deltaTime;
+            }
             finalSpeed = speedInit * acceleration;
-            transform.position += new Vector3(-finalSpeed, 0,0) * Time.deltaTime;
         }
+        else{
+            if(speedInit > 0){
+                speedInit-=Time.deltaTime;
+            }
+            
+        }
+        transform.position += new Vector3(finalSpeed, 0,0) * Time.deltaTime;
     }
 
     void LateUpdate(){
-        if(maps.Count >=15){
-            Destroy(maps[0]);
-            maps.RemoveAt(0);
+        if(activemaps.Count >=3){
+            Destroy(activemaps[0]);
+            activemaps.RemoveAt(0);
         }
     }
 }
