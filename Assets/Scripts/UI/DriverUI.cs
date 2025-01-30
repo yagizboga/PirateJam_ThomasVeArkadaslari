32cf303916bdcb729dcd19a6b72f247259ace3aa
Mert Kavilcioğlu <mertkavilci@gmail.com>
2025-01-30 17:27:03 +0300

    WEBGL

--- Assets/Scripts/UI/DriverUI.cs
+++ Assets/Scripts/UI/DriverUI.cs
@@ -6,6 +6,21 @@ public class DriverUI : MonoBehaviour
     public Image trainHealthBar;
     private float healthAmount = 100f;
 
+    public GameObject stopSignActive;
+    public GameObject stopSignDeactive;
+
+    public GameObject repairSignActive;
+    public GameObject repairSignDeactive;
+
+    private void Start()
+    {
+        stopSignActive.SetActive(false);
+        repairSignActive.SetActive(false);
+
+        stopSignDeactive.SetActive(true);
+        repairSignDeactive.SetActive(true);
+    }
+
 
     public void UpdateTrainHealthUI(float health)
     {
