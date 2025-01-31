9f92bd86fcf071c16368a670d8a3db891f62a7cc
Mert Kavilcioğlu <mertkavilci@gmail.com>
2025-01-31 15:46:15 +0300

    SON

---
+++ Assets/Scripts/UI/STARTTT.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+public class STARTTT : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
